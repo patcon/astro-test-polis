110abd3e1a2a9aaf51688767e9e6f2dc746e0c36
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-01-03 23:44:17 -0500

    Remove netlify for now. Back to static website.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
+// import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  adapter: netlify({
-    edgeMiddleware: true
-  })
+  output: 'static',
+  // adapter: netlify({
+  //   edgeMiddleware: true
+  // }),
 });
