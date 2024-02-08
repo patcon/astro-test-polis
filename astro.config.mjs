7046f48cc3d49fdd8c6c30a707da1fb6b3c8db60
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-02-08 15:03:23 -0600

    Added MDX and about and faq page stubs.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   // adapter: netlify({
   //   edgeMiddleware: true
   // }),
-  integrations: [tailwind()]
+  integrations: [tailwind(), mdx()]
 });
