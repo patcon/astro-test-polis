c8d0f25405f30f5ce05bb38ead81e7ab0b5f3ae7
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-02-12 03:35:34 -0600

    Added stub of IP logging call.

--- netlify/functions/log-xid.mjs
+++ netlify/functions/log-xid.mjs
@@ -1,9 +1,17 @@
 export default async (req, context) => {
-  const jsonResponse = JSON.stringify(context, null, 2);
-  console.log(jsonResponse);
-  return new Response(jsonResponse);
+  switch (req.method) {
+    case 'POST':
+      const { polisXid, mailchimpUid } = await req.json();
+      const responseObj = Object.assign({}, context, { polisXid, mailchimpUid });
+      const prettyResponse = JSON.stringify(responseObj, null, 2);
+      console.log(prettyResponse);
+      return new Response(prettyResponse);
+
+    default:
+      return new Response('Method Not Allowed', { status: 405 });
+  }
 };
 
 export const config = {
-  path: "/log-xid"
+  path: "/api/log-xid"
 };
