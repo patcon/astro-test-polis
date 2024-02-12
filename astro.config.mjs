65f5455fc5deb9a6997a2d83e7b93d57e2f28eb2
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-02-11 22:17:34 -0600

    Added a test netlify function.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from 'astro/config';
 // import netlify from "@astrojs/netlify";
 
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,6 @@ export default defineConfig({
   // adapter: netlify({
   //   edgeMiddleware: true
   // }),
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  adapter: netlify()
 });
