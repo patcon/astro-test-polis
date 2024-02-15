b8b66119681e99512d07e4ba86a17a3ab419bee6
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-02-15 14:38:56 -0600

    Added PT Sans font as base.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
 	content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
     './node_modules/flowbite/**/*.js',
   ],
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        sans: ['PT Sans', ...defaultTheme.fontFamily.sans],
+        serif: ['PT Serif', ...defaultTheme.fontFamily.serif],
+        chaney: ['Chaney', 'Arial', 'sans-serif'],
+      },
+    },
 	},
 	plugins: [
     require('@tailwindcss/typography'),
