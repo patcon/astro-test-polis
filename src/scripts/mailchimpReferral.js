f40ce486d6b3ce66cdc6456e33a6b77079c1646a
Patrick Connolly <patrick.c.connolly@gmail.com>
2024-02-20 10:10:24 -0600

    Added flag to determine if mailchimp ID needs migration.

--- src/scripts/mailchimpReferral.js
+++ src/scripts/mailchimpReferral.js
@@ -1,3 +1,9 @@
+const MAILCHIMP_BAD_UID = "d7c6d2a743011f3253613888f"
+const prevMailchimpUid = localStorage.getItem('mailchimpUid')
+if (prevMailchimpUid === MAILCHIMP_BAD_UID) {
+  localStorage.setItem('needsMigration', "true")
+}
+
 // Transfer Mailchimp UID from URL query parameter to localStorage.
 const url = new URL(document.location);
 
